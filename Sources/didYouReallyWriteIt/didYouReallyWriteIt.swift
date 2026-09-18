2036041e8e8eb9dbffac25286625c5314b519417
headStyleColorRed <headStyleColorRed@gmail.com>
2026-09-18 20:50:13 +0300

    Removing unnecessary comments

--- Sources/didYouReallyWriteIt/didYouReallyWriteIt.swift
+++ Sources/didYouReallyWriteIt/didYouReallyWriteIt.swift
@@ -1,16 +1,7 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import MLX
 
 @main
 struct DidYouReallyWriteIt {
-    /// **Terms**
-	/// parameter = where you are horizontally
-	/// loss      = how high up the mountain you are
-	/// gradient  = slope beneath your feet
-	/// learning rate = size of your step
-	/// gradient descent = repeatedly walking downhill
     static func main() {
         let inputValue = MLXArray(2.0)
         let targetValue = MLXArray(10.0)
@@ -38,14 +29,6 @@ struct DidYouReallyWriteIt {
 
             parameter = parameter - learningRate * gradient
             loss = lossMethod(parameter)
-
-            print("Iteration:", iteration)
-            print("Parameter:", parameter.item(Float.self))
-            print("Gradient:", gradient.item(Float.self))
-            print("Loss:", loss.item(Float.self))
-            print("----------")
         }
-
-        print(parameter)
     }
 }
